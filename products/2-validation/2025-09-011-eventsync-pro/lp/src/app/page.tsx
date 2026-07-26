49ac9d0b75d34f60a531fe224c60488ba796c171
佐藤圭吾 <k0127s@gmail.com>
2026-07-26 12:26:56 +0900

    chore: 旧検証LPと生成coverageを削除

--- products/2-validation/2025-09-011-eventsync-pro/lp/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import LandingPageTemplate from '@/components/templates/LandingPageTemplate'
-import config from '../../lp-config.json'
-import { TemplateConfig } from '@/types/template'
-
-export default function HomePage() {
-  return <LandingPageTemplate config={config as unknown as TemplateConfig} />
-}
